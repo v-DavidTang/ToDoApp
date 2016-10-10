4970ea0ff80dba03ad22bf742331fefedfb19c5e
v-DavidTang <857771518@qq.com>
2016-10-10 10:07:09 +0800

    change the environment to China

--- ARMTemplates/deploy.ps1
+++ ARMTemplates/deploy.ps1
@@ -3,7 +3,7 @@
 	[string] $SlotName = "Staging",
 	[string] $TemplateFile = "ProdAndStage.json",
 	[string] $TemplateParameterFile = "param.json",
-	[string] $RepoUrl = "https://github.com/azure-appservice-samples/ToDoApp.git",
+	[string] $RepoUrl = "https://github.com/v-DavidTang/ToDoApp.git",
 	[string] $Branch = "master"
 )
 
